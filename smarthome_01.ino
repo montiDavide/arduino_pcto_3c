3d8c1358e3331ef54dfa3352134ed977a8877e4f
montiDavide <123242008+montiDavide@users.noreply.github.com>
2023-01-25 10:45:03 +0100

    Update smarthome_01.ino

--- smarthome_01.ino
+++ smarthome_01.ino
@@ -9,6 +9,12 @@ da scaricare!
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
+#if defined(ARDUINO) && ARDUINO >= 100
+#define printByte(args)  write(args);
+#else
+#define printByte(args)  print(args,BYTE);
+#endif
+
 DHT11 dht11(D7);                    // creating the object sensor on pin 'D7'
 LiquidCrystal_I2C lcd(0x27,16,2);   // 0x3F = 63 // usa 0x27 se non va
 Servo Garage;
@@ -27,7 +33,7 @@ int garagedelay;
 float dist_cm;
 
 // config luce
-int const LIM_LUCE = 80;
+int const LIM_LUCE = 30;
 // config Dist. Garage
 float const DISTANZA_NORM = 20 ;
 int const DISTANZA_VARIANZ = 25; // % della norm.
@@ -39,12 +45,20 @@ struct {
 }GARAGE_STATE;
 
 bool garage = false;
+// caratteri x icona luce ext
+uint8_t luce_asx[8]={0b00000,0b00000,0b00000,0b00000,0b00001,0b00010,0b00100};
+uint8_t luce_adx[8]={0b00001,0b01010,0b00100,0b00000,0b10000,0b01000,0b00100};
+uint8_t luce_bsx[8]={0b00100,0b00110,0b00010,0b00010,0b00001,0b00011,0b00001};
+uint8_t luce_bdx[8]={0b00100,0b01100,0b01000,0b01000,0b10000,0b11000,0b10000};
+
+uint8_t check[8] = {0x0,0x1,0x3,0x16,0x1c,0x8,0x0};
 
 float getDist_cm();
 bool ApriGarage(float dist);
 void openGarage();
 void closeGarage();
 void manageGarage(float dist_cm);
+void stampa_lapada_lcd();
 
 void setup()
 {
@@ -54,13 +68,23 @@ void setup()
   
   lcd.init();                      // initialize the lcd 
   lcd.backlight();
-  
+
+  lcd.home();
+
   Garage.attach(MOTORE_1);
 
   // robo x distanza  
   pinMode(TRIG_DIST, OUTPUT);
   pinMode(ECHO_DIST, INPUT);
   garagedelay = tempo_garage;
+
+  lcd.createChar(0, luce_asx);
+  lcd.createChar(1, luce_adx);
+  lcd.createChar(2, luce_bsx);
+  lcd.createChar(3, luce_bdx);
+
+  lcd.createChar(4,check);
+
 }
 
 void loop()
@@ -90,16 +114,24 @@ void loop()
   //Serial.println(analogRead(TASTO_GARAGE));
 
 
-
+  // prepara buffer
+  snprintf(buffer[0], 16,"C %03i, H:%02i \0", temp_read, umidita_read); 
+  snprintf(buffer[1], 16,"Luce : %05i \0", fotoresist);
+  
+  //svuota schermo
+  lcd.clear();
+  
   // enable outside led
   if (fotoresist<LIM_LUCE){
-    digitalWrite(LED_EXT, HIGH);   
+    digitalWrite(LED_EXT, HIGH);
+    stampa_lapada_lcd();
+    
   }else{
     digitalWrite(LED_EXT, LOW);
+    
   }
   // scrivi a schermo
-  snprintf(buffer[0], 16,"C %03i, H:%02i \0", temp_read, umidita_read); 
-  snprintf(buffer[1], 16,"Luce : %05i \0", fotoresist); 
+  
   lcd.setCursor(0, 0);
   lcd.print( buffer[0] ); 
   lcd.setCursor(0, 1);
@@ -107,7 +139,7 @@ void loop()
   
   manageGarage(dist_cm);
 
-   delay(1000);                // Wait 1 second before looping
+  delay(1000);                // Wait 1 second before looping
 }
 
 float getDist_cm(){
@@ -175,4 +207,19 @@ void manageGarage(float dist_cm){
     }
   }
 
+}
+
+// icona_luce
+void stampa_lapada_lcd(){
+  lcd.setCursor(14, 0);
+  lcd.printByte(0);
+
+  lcd.setCursor(15, 0);
+  lcd.printByte(1);
+
+  lcd.setCursor(14, 1);
+  lcd.printByte(2);
+
+  lcd.setCursor(15, 1);
+  lcd.printByte(3);
 }
